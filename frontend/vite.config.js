55d23c3acac7acc18dde9d8faeb5deed54ccf488
yashkindre1 <yash.kindre@outlook.com>
2025-02-21 16:48:13 +0530

    commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-    base: "/book-store-mern-app/",
+  build: {
+    outDir: 'dist', // Ensures output is in dist folder
+  },
 })
